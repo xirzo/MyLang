54547f8efc062f69e5dc064ae069a99a1be9efd3
xirzo <easttem@gmail.com>
2025-06-26 00:00:22 +0300

    feat: parse ident

--- src/lexer.zig
+++ src/lexer.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 pub const LexemeTag = enum {
+    illegal,
     ident,
     assign,
     number,
@@ -9,6 +10,7 @@ pub const LexemeTag = enum {
 };
 
 pub const Lexeme = union(LexemeTag) {
+    illegal: void,
     ident: []const u8,
     assign: u8,
     number: i64,
@@ -18,24 +20,36 @@ pub const Lexeme = union(LexemeTag) {
 
 pub const Lexer = struct {
     src: []const u8,
-    cur_pos: i64,
-    read_pos: i64,
+    cur_char: u8,
+    cur_pos: usize,
+    read_pos: usize,
 
     pub fn init(src: []const u8) Lexer {
         return .{
             .src = src,
-            .cur_pos = -1,
-            .read_pos = 0,
+            .cur_char = undefined,
+            .cur_pos = 0,
+            .read_pos = 1,
         };
     }
 
     fn read_char(l: *Lexer) void {
         if (l.read_pos >= l.src.len) {
+            l.cur_char = undefined;
             return;
         }
 
         l.cur_pos += 1;
         l.read_pos += 1;
+        l.cur_char = l.src[l.cur_pos];
+    }
+
+    fn peek_char(l: *Lexer) u8 {
+        if (l.read_pos > l.src.len) {
+            return undefined;
+        }
+
+        return l.src[l.read_pos];
     }
 
     fn skip_whitespaces(l: *Lexer) void {
@@ -44,11 +58,29 @@ pub const Lexer = struct {
         }
     }
 
+    fn parse_ident(l: *Lexer) Lexeme {
+        const start_index: usize = l.cur_pos;
+
+        while (!std.ascii.isAlphabetic(l.peek_char())) {
+            l.read_char();
+        }
+
+        return Lexeme{ .ident = l.src[start_index..l.cur_pos] };
+    }
+
     pub fn next(l: *Lexer) Lexeme {
         l.skip_whitespaces();
 
-        return .{
-            .ident = "loool",
+        return switch (l.peek_char()) {
+            ';' => Lexeme{ .semicolon = l.peek_char() },
+
+            else => {
+                if (std.ascii.isAlphabetic(l.peek_char())) {
+                    return l.parse_ident();
+                }
+
+                return Lexeme{ .illegal = {} };
+            },
         };
     }
 };
